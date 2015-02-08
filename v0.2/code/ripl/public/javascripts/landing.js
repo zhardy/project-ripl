97be462e853d6b3de5d8564b23cb22af06883823
tvachnad <tvachnad@umass.edu>
2015-02-08 15:24:32 -0500

    works great on mobile

--- v0.2/code/ripl/public/javascripts/landing.js
+++ v0.2/code/ripl/public/javascripts/landing.js
@@ -1,9 +1,5 @@
-//if(('.name').width()<63)
-console.log($('.name').width());
-console.log($('#email').width());
-console.log($('button').width());
 if($('.name').width()<71){
-	$('.name').css("width", 71);
+	$('.name').width(71);
 }
 if($('#email').width()<158){
 	$('#email').width(147);
