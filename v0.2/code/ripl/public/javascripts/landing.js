77eb623e625af0cde557030cf8352c7065e4bda4
tvachnad <tvachnad@umass.edu>
2015-02-16 18:35:26 -0500

    about page ready

--- v0.2/code/ripl/public/javascripts/landing.js
+++ v0.2/code/ripl/public/javascripts/landing.js
@@ -7,3 +7,33 @@ if($('#email').width()<158){
 if($('button').width()<45){
 	$('button').width(45);
 }
+
+$(document).ready(function(){
+pos=0;
+});
+
+$('body').bind('mousewheel', function(e){
+	
+	$('body').stop();
+});
+
+(function() {        
+    var timer;
+    $(window).bind('scroll',function () {
+        clearTimeout(timer);
+        timer = setTimeout( refresh , 500 );
+    });
+    var refresh = function () { 
+        // do stuff
+
+        console.log('Stopped Scrolling'); 
+        var scroll = $(window).scrollTop();
+        console.log("offset = "+ scroll); 
+        var height = $('body').height();
+        if(scroll<320)
+        	$("html, body").animate({scrollTop: "0"}, 1000);
+        else
+        $("html, body").animate({scrollTop: height}, 1000);	
+    };
+})();
+
